644873d26fd6527cabaa7ae5170a93b57659ee7d
baiaimin <15130794497@163.com>
2020-03-17 10:43:28 +0800

    提交体侧初始项目

---
+++ src/views/herry-project/test.vue
@@ -0,0 +1,35 @@
+<!--
+*@描述:test
+*@版本:V1.0
+*@作者:白爱民
+*@Date:2019年12月11日20:16:26
+*@最后修改人:herry
+*@LastEditTime:2019年12月11日20:16:31
+*@说明：-->
+<template>
+  <div>
+    <van-field readonly clickable :value="value" @touchstart.native.stop="show = true" />
+
+    <van-number-keyboard v-model="value" :show="show" :maxlength="6" @blur="show = false" />
+  </div>
+</template>
+<style scoped>
+</style>
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+      value: ""
+    };
+  },
+  mounted() {},
+  methods: {
+    // newitems() {
+    //   var searchArr = [];
+    //   if (this.searchMessage === "") {
+    //   }
+    // }
+  }
+};
+</script>
